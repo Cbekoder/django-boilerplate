f8c61c8912999e6f3f764ce4d0335115b232f1b2
cbekoder <cbekoder@gmail.com>
2025-02-17 23:16:59 +0500

    changing

--- core/settings/base.py
+++ core/settings/base.py
@@ -18,7 +18,7 @@ from core.jazzmin_conf import *  # noqa
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Reading .env file
